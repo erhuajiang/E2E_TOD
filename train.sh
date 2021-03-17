285750e4534045751ca2ea1c58472f0d2b3b0c05
fengyue <fengyue.fy@bytedance.com>
2021-03-17 05:44:35 +0000

    action

--- train.sh
+++ train.sh
@@ -1,13 +1,13 @@
 
 export CUDA_DEVICE_ORDER=PCI_BUS_ID
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 
 mode=$1
 dataset=$2
 
-if [ $mode == "sl" ]; then
+if [ $mode = "sl" ]; then
   python -u supervised.py --config_name sl_hdno_$dataset
-elif [ $mode == "rl" ]; then
+elif [ $mode = "rl" ]; then
   python -u reinforce.py --config_name rl_hdno_$dataset
 else
   echo "wrong input"
